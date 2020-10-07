29b3ff08055a0075f4226973ac2c91729fb9ea23
Jasdev Singh <jasdevsingh@Jasdevs-MacBook-Pro.local>
2020-10-07 16:01:16 -0400

    Adds `introspectTextView`.

--- Introspect/ViewExtensions.swift
+++ Introspect/ViewExtensions.swift
@@ -16,7 +16,7 @@ extension View {
 #if canImport(UIKit)
 @available(iOS 13.0, tvOS 13.0, macOS 15.0, *)
 extension View {
-    
+
     /// Finds a `TargetView` from a `SwiftUI.View`
     public func introspect<TargetView: UIView>(
         selector: @escaping (IntrospectionUIView) -> TargetView?,
@@ -27,24 +27,24 @@ extension View {
             customize: customize
         ))
     }
-    
+
     /// Finds a `UINavigationController` from any view embedded in a `SwiftUI.NavigationView`.
     public func introspectNavigationController(customize: @escaping (UINavigationController) -> ()) -> some View {
         return inject(UIKitIntrospectionViewController(
             selector: { introspectionViewController in
-                
+
                 // Search in ancestors
                 if let navigationController = introspectionViewController.navigationController {
                     return navigationController
                 }
-                
+
                 // Search in siblings
                 return Introspect.previousSibling(containing: UINavigationController.self, from: introspectionViewController)
             },
             customize: customize
         ))
     }
-    
+
     /// Finds the containing `UIViewController` of a SwiftUI view.
     public func introspectViewController(customize: @escaping (UIViewController) -> ()) -> some View {
         return inject(UIKitIntrospectionViewController(
@@ -57,24 +57,24 @@ extension View {
     public func introspectTabBarController(customize: @escaping (UITabBarController) -> ()) -> some View {
         return inject(UIKitIntrospectionViewController(
             selector: { introspectionViewController in
-                
+
                 // Search in ancestors
                 if let navigationController = introspectionViewController.tabBarController {
                     return navigationController
                 }
-                
+
                 // Search in siblings
                 return Introspect.previousSibling(ofType: UITabBarController.self, from: introspectionViewController)
             },
             customize: customize
         ))
     }
-    
+
     /// Finds a `UITableView` from a `SwiftUI.List`, or `SwiftUI.List` child.
     public func introspectTableView(customize: @escaping (UITableView) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
     }
-    
+
     /// Finds a `UITableViewCell` from a `SwiftUI.List`, or `SwiftUI.List` child. You can attach this directly to the element inside the list.
     public func introspectTableViewCell(customize: @escaping (UITableViewCell) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
@@ -84,36 +84,41 @@ extension View {
     public func introspectScrollView(customize: @escaping (UIScrollView) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
     }
-    
+
     /// Finds a `UITextField` from a `SwiftUI.TextField`
     public func introspectTextField(customize: @escaping (UITextField) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
+      /// Finds a `UITextView` from a `SwiftUI.TextEditor`.
+      public func introspectTextView(customize: @escaping (UITextView) -> ()) -> some View {
+            return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
+      }
+
     /// Finds a `UISwitch` from a `SwiftUI.Toggle`
     @available(tvOS, unavailable)
     public func introspectSwitch(customize: @escaping (UISwitch) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `UISlider` from a `SwiftUI.Slider`
     @available(tvOS, unavailable)
     public func introspectSlider(customize: @escaping (UISlider) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `UIStepper` from a `SwiftUI.Stepper`
     @available(tvOS, unavailable)
     public func introspectStepper(customize: @escaping (UIStepper) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `UIDatePicker` from a `SwiftUI.DatePicker`
     @available(tvOS, unavailable)
     public func introspectDatePicker(customize: @escaping (UIDatePicker) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `UISegmentedControl` from a `SwiftUI.Picker` with style `SegmentedPickerStyle`
     public func introspectSegmentedControl(customize: @escaping (UISegmentedControl) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
@@ -124,7 +129,7 @@ extension View {
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
 @available(macOS 15.0, *)
 extension View {
-    
+
     /// Finds a `TargetView` from a `SwiftUI.View`
     public func introspect<TargetView: NSView>(
         selector: @escaping (IntrospectionNSView) -> TargetView?,
@@ -135,7 +140,7 @@ extension View {
             customize: customize
         ))
     }
-    
+
     /// Finds a `NSTableView` from a `SwiftUI.List`, or `SwiftUI.List` child.
     public func introspectTableView(customize: @escaping (NSTableView) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
@@ -150,27 +155,27 @@ extension View {
     public func introspectScrollView(customize: @escaping (NSScrollView) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.ancestorOrSibling, customize: customize)
     }
-    
+
     /// Finds a `NSTextField` from a `SwiftUI.TextField`
     public func introspectTextField(customize: @escaping (NSTextField) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `NSSlider` from a `SwiftUI.Slider`
     public func introspectSlider(customize: @escaping (NSSlider) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `NSStepper` from a `SwiftUI.Stepper`
     public func introspectStepper(customize: @escaping (NSStepper) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `NSDatePicker` from a `SwiftUI.DatePicker`
     public func introspectDatePicker(customize: @escaping (NSDatePicker) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
     }
-    
+
     /// Finds a `NSSegmentedControl` from a `SwiftUI.Picker` with style `SegmentedPickerStyle`
     public func introspectSegmentedControl(customize: @escaping (NSSegmentedControl) -> ()) -> some View {
         return introspect(selector: TargetViewSelector.sibling, customize: customize)
